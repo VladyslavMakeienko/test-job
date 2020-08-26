1a20e9817dcde819015ebc87046581ed4cdf011d
VladyslavMakeienko <vsmakeienko@gmail.com>
2020-08-26 13:11:49 +0300

    Updates

---
+++ precache-manifest.bd39567e1c61360230e96f1c09ab7333.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f8ce7b3e63e11f7fed70ca791bfc0c",
+    "url": "/test-job/index.html"
+  },
+  {
+    "revision": "09887f0421aac9a9ab6d",
+    "url": "/test-job/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "966f77d91b84e3115b54",
+    "url": "/test-job/static/js/2.58c09c24.chunk.js"
+  },
+  {
+    "revision": "5b8b49146ea7ff4abb4f22a9d28772fe",
+    "url": "/test-job/static/js/2.58c09c24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09887f0421aac9a9ab6d",
+    "url": "/test-job/static/js/main.c09d89e5.chunk.js"
+  },
+  {
+    "revision": "278496712c6f9f896fd7",
+    "url": "/test-job/static/js/runtime-main.64410cb9.js"
+  },
+  {
+    "revision": "4e51e3272554affdca46802ae9be2b94",
+    "url": "/test-job/static/media/ArrowLeft.4e51e327.jpg"
+  },
+  {
+    "revision": "b71a1f7795bd13451affbbf44756cc99",
+    "url": "/test-job/static/media/ArrowRight.b71a1f77.jpg"
+  },
+  {
+    "revision": "5bd7ac7b1fec7b233cf488286d3a5332",
+    "url": "/test-job/static/media/galleryFirst.5bd7ac7b.jpg"
+  },
+  {
+    "revision": "9260bbd53fb9739c8b951b65871b9e2e",
+    "url": "/test-job/static/media/gallerySecond.9260bbd5.jpg"
+  },
+  {
+    "revision": "3bd03da03e86b0e2c0d094d49b041a60",
+    "url": "/test-job/static/media/galleryThird.3bd03da0.jpg"
+  },
+  {
+    "revision": "e2bc47051aa4abb29adc87affef98295",
+    "url": "/test-job/static/media/imageForSiteSecond.e2bc4705.jpg"
+  },
+  {
+    "revision": "edcf9ae445044c063e757ebf0b29d78f",
+    "url": "/test-job/static/media/imageForSiteThird.edcf9ae4.jpg"
+  },
+  {
+    "revision": "80cbf113ec1c6db85d231ca92b745398",
+    "url": "/test-job/static/media/imageForSlider.80cbf113.jpg"
+  }
+]);
